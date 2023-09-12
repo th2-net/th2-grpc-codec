b6e0e4869654b6384a85be81c797ee4f08813a8a
Oleg Smelov <45400511+lumber1000@users.noreply.github.com>
2023-09-12 14:47:48 +0400

    Gradle, dependencies and workflows updated (#4)
    gradle, dependencies and workflows updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# th2 gRPC codec library (0.0.2)
+# th2 gRPC codec library (0.1.0)
 
 This library contains proto messages and `Codec` service with RPC methods that are used in [th2 codec](https://github.com/th2-net/th2-codec "th2-codec"). See [codec.proto](src/main/proto/th2_grpc_codec/codec.proto "codec.proto") file for details. <br>
 Tool generates code from `.proto` files and uploads built packages (`.proto` files and generated code) to the specified repositories.
@@ -37,6 +37,10 @@ If you wish to manually create and publish a package for Python:
     `PYPI_REPOSITORY_URL`, `PYPI_USER` and `PYPI_PASSWORD` are parameters for publishing.
 
 ## Release notes
+
+### 0.1.0
+
++ Updated grpc-common 4.3.0
 
 ### 0.0.2
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#   Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+#   Copyright 2020-2023 Exactpro (Exactpro Systems Limited)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ class ProtoGenerator(Command):
 
     def run(self):
         proto_path = os.path.abspath('src/main/proto')
-        gen_path = os.path.abspath('src/gen/main/python')
+        gen_path = os.path.abspath('build/generated/source/proto/main/services/python')
 
         if not os.path.exists(gen_path):
             os.makedirs(gen_path)
@@ -72,8 +72,7 @@ class CustomDist(sdist):
     def run(self):
         copy_tree(f'src/main/proto/{package_name}', package_name)
 
-        copy_tree(f'src/gen/main/python/{package_name}', package_name)
-        copy_tree(f'src/gen/main/services/python/{package_name}', package_name)
+        copy_tree(f'build/generated/source/proto/main/services/python/{package_name}', package_name)
         Path(f'{package_name}/__init__.py').touch()
         Path(f'{package_name}/py.typed').touch()
 
@@ -121,7 +120,8 @@ setup(
     license='Apache License 2.0',
     python_requires='>=3.7',
     install_requires=[
-        'th2-grpc-common~=3.9.0'
+        'th2-grpc-common==4.3.0.dev0',
+        'mypy-protobuf==3.4'
     ],
     packages=packages,
     package_data=package_data,
